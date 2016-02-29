b3256a0f11c4ed351fb2d65059c64a25599e5e2a
Michael McAdam <mjm369@uclive.ac.nz>
2016-03-01 11:15:52 +1300

    Added log and build folder

---
+++ build/CMakeFiles/Track_Insect.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ucnavi2/fyp/src/autopilot_interface.cpp" "/home/ucnavi2/fyp/build/CMakeFiles/Track_Insect.dir/src/autopilot_interface.cpp.o"
+  "/home/ucnavi2/fyp/src/camera_interface.cpp" "/home/ucnavi2/fyp/build/CMakeFiles/Track_Insect.dir/src/camera_interface.cpp.o"
+  "/home/ucnavi2/fyp/src/insect.cpp" "/home/ucnavi2/fyp/build/CMakeFiles/Track_Insect.dir/src/insect.cpp.o"
+  "/home/ucnavi2/fyp/src/main.cpp" "/home/ucnavi2/fyp/build/CMakeFiles/Track_Insect.dir/src/main.cpp.o"
+  "/home/ucnavi2/fyp/src/mavlink_interface.cpp" "/home/ucnavi2/fyp/build/CMakeFiles/Track_Insect.dir/src/mavlink_interface.cpp.o"
+  "/home/ucnavi2/fyp/src/serial_interface.cpp" "/home/ucnavi2/fyp/build/CMakeFiles/Track_Insect.dir/src/serial_interface.cpp.o"
+  "/home/ucnavi2/fyp/src/threshold.cpp" "/home/ucnavi2/fyp/build/CMakeFiles/Track_Insect.dir/src/threshold.cpp.o"
+  "/home/ucnavi2/fyp/src/ui_input.cpp" "/home/ucnavi2/fyp/build/CMakeFiles/Track_Insect.dir/src/ui_input.cpp.o"
+  "/home/ucnavi2/fyp/src/ui_output.cpp" "/home/ucnavi2/fyp/build/CMakeFiles/Track_Insect.dir/src/ui_output.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include/opencv"
+  "/usr/local/include"
+  "../src/include"
+  "/usr/include/flycapture"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Track_Insect.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Track_Insect.dir/src/autopilot_interface.cpp.o"
+  "CMakeFiles/Track_Insect.dir/src/mavlink_interface.cpp.o"
+  "CMakeFiles/Track_Insect.dir/src/camera_interface.cpp.o"
+  "CMakeFiles/Track_Insect.dir/src/threshold.cpp.o"
+  "CMakeFiles/Track_Insect.dir/src/insect.cpp.o"
+  "CMakeFiles/Track_Insect.dir/src/main.cpp.o"
+  "CMakeFiles/Track_Insect.dir/src/ui_output.cpp.o"
+  "CMakeFiles/Track_Insect.dir/src/serial_interface.cpp.o"
+  "CMakeFiles/Track_Insect.dir/src/ui_input.cpp.o"
+  "Track_Insect.pdb"
+  "Track_Insect"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Track_Insect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
